69a7debecc8312c8c5bc77cc1d55a396aa080881
sefayehalilova <43229815+sefayehalilova@users.noreply.github.com>
2019-03-26 22:11:27 +0300

    Create Задача 44.lisp

---
+++ Задача 44.lisp
@@ -0,0 +1,17 @@
+;Определите функцию, проверяющую, является ли одно дерево поддеревом второго.
+
+(defun trees (v1 v2)
+    ((lambda (x)(and (setq first (car x)) (setq last (cdr x))))v1)
+     (cond((null v1) nil)
+       ((atom first) (trees last v2))
+       ( t(if (equal first v2) 
+           t(or (trees first v2) (trees last v2)))
+       )
+      )
+ )
+
+
+;(print(trees '((a (b(c))) h m) '(a (b (c)))))
+;T
+;(print(trees '((a (b(c))) h (m(l))) '(a b c))) 
+;NIL
