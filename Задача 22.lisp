4e3d867f51bb92aeb75987526ec3af98cb8dd786
sefayehalilova <43229815+sefayehalilova@users.noreply.github.com>
2019-04-02 19:13:38 +0300

    Update Задача 22.lisp

--- Задача 22.lisp
+++ Задача 22.lisp
@@ -1,8 +1,15 @@
 ;Определите функцию, которая обращает список (а б с) и разбивает его на уровни (((с) б) а). 
 
-(defun obr (abc)
-	(list(list (cddr abc) (cadr abc)) (car abc))
+(defun rev-lev(str)
+    ((lambda(last) 
+     (if(null last)str
+        (list(rev-lev last)(car str))
+      )
+      )
+     (cdr str)
+    )
 )
-  
-;(print(obr `(a b c)))
-;(((C) B) A)
+
+	 
+;(print(rev-lev '(g j k l m)))
+;(((((M) L) K) J) G)
