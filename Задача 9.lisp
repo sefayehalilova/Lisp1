a31ddc0d8f7820e5478fcda887e80b07df82ba10
sefayehalilova <43229815+sefayehalilova@users.noreply.github.com>
2019-04-03 16:39:06 +0300

    Update Задача 9.lisp

--- Задача 9.lisp
+++ Задача 9.lisp
@@ -1,26 +1,27 @@
 ;Определите функцию, разделяющую исходный список на два подсписка. 
 ;В первый из них должны попасть элементы с нечетными номерами, во второй - элементы с четными номерами.
 
-(defun broke (str) 
-    ((lambda (first) 
-       (cond ((null first) str) 
-                      (t (setq nstr (broke (cddr str))) 
-                        (list 
-                         (cons first (car nstr)) 
-                         (cons (cadr str) (cadr nstr))
-                         )
-                      ) 
-       ) 
-     )            
-      (car str)
- 
+(defun broke ( str )
+    ((lambda (second rest)
+    (if str
+        (list
+            (cons (car str) (car (broke rest)))
+             (if second
+                (cons second (cadr (broke rest)))
+             )
+        ) 
     )
+      )
+     (cadr str) (cddr str) 
+     )
 )
+	
 
+;( print (broke '( 8 )))
+;( print (broke '( 1 2 5 6 )))
+;( print (broke '( a b c d k f )))
 
+;((8) NIL) 
+;((1 5) (2 6)) 
+;((A C K) (B D F)) 
 
-;(print (broke `(6 8 7 3 2 6)))
-;((6 7 2) (8 3 6))
-
-;(print (broke `()))
-;NIL
