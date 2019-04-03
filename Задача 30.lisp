612ba7c809c169fca818625ce327dc4356ba4901
sefayehalilova <43229815+sefayehalilova@users.noreply.github.com>
2019-04-03 18:37:13 +0300

    Update Задача 30.lisp

--- Задача 30.lisp
+++ Задача 30.lisp
@@ -3,3 +3,18 @@
 ;(ВЫЧИСЛИ '((-2 + 4) * 3))
 ;6
    
+(defun calculate (str)
+(eval ( prefix str ))
+ )
+
+(defun prefix (lst)
+   (cond ((null lst) nil)
+         ((atom lst) lst)
+         (t (list (prefix (cadr lst)) (prefix (car lst) ) (prefix (caddr lst)))
+          )
+    )
+ )
+    
+
+(print  (prefix '((-4 + 4 ) *  3 ))) 
+(print (calculate '((4 + 4 ) *  3 ))) 
