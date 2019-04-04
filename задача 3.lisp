b040c0dfacc95ea2a0c80d76ec09e01fbf6e029a
sefayehalilova <43229815+sefayehalilova@users.noreply.github.com>
2019-04-04 09:08:30 +0300

    Update задача 3.lisp

--- задача 3.lisp
+++ задача 3.lisp
@@ -3,8 +3,8 @@
 (defun repl (str &key x y) 
     ((lambda (first last) 
        (cond ((null str) nil) 
-         ((equal first x) (cons y (repl last :x :y)))
-         (t(cons first (repl last :x :y)))             
+         ((equal first x) (cons y (repl last :x x :y  y)))
+         (t(cons first (repl last :x x :y y)))             
        ) 
       )            
       (car str) (cdr str)  
@@ -14,7 +14,7 @@
 
 
 
-;(print(repl '(2 4 3 6 7 ) 6 7))
+(print(repl '(2 4 3 6 7 ) :x  6 :y 7))
 ;(2 4 3 7 7)
-;(print(repl '(1 1 0 5 1 ) 1 'd))
+;(print(repl '(1 1 0 5 1 ) :x 1 :x 'd))
 ;(D D 0 5 D) 
